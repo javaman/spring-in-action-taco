c67808a9087b367e4a1744ba5a09b723570f07ea
Maksim Krylov <krylovm@ae.com>
2022-02-19 09:37:18 +0300

    Initial commit

---
+++ src/main/java/xyz/javaman/taco/SpringInActionTacoApplication.java
@@ -0,0 +1,13 @@
+package xyz.javaman.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInActionTacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInActionTacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/javaman/taco/SpringInActionTacoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.javaman.taco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionTacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
