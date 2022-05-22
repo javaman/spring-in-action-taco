daff43f1773dfe357532458c37978aaa48237311
Maksim Krylov <kmv20@yandex.ru>
2022-05-22 13:50:46 +0300

    2.4 section is ready

--- src/main/java/xyz/javaman/taco/SpringInActionTacoApplication.java
+++ src/main/java/xyz/javaman/taco/SpringInActionTacoApplication.java
@@ -2,12 +2,18 @@ package xyz.javaman.taco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringInActionTacoApplication {
+public class SpringInActionTacoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringInActionTacoApplication.class, args);
 	}
 
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 }
